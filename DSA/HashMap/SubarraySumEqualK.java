d3d65a1c63f6656a4ac4c6c9761eed673073af5a
Prateek Kumar <71647878+geek-prateek@users.noreply.github.com>
2023-06-03 23:59:12 +0530

    Hash

---
+++ DSA/HashMap/SubarraySumEqualK.java
@@ -0,0 +1,22 @@
+package Hashing;
+
+import java.util.HashMap;
+
+public class SubarraySumEqualK {
+    public static void main(String[] args) {
+        int arr[] = {1,2,3};
+        int k = 3;
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        hm.put(0,1);
+        int sum =0;
+        int ans = 0;
+        for(int j=0; j< arr.length; j++){ //O(n)
+            sum+=arr[j];
+            if(hm.containsKey(sum-k)){
+                ans += hm.get(sum-k);
+            }
+            hm.put(sum, hm.getOrDefault(sum, 0)+1);
+        }
+        System.out.println(ans);
+    }
+}
