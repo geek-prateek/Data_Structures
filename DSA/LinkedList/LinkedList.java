0d45f3ded69942ac8aa265c6f83f7dd565f1077d
Prateek Kumar <71647878+geek-prateek@users.noreply.github.com>
2023-06-13 17:43:10 +0530

    Update

--- DSA/LinkedList/LinkedList.java
+++ DSA/LinkedList/LinkedList.java
@@ -4,61 +4,184 @@ public class LinkedList {
     public static class Node{
         int data;
         Node next;
-        Node(int val){
-            this.data = val;
+
+        public Node(int data){
+            this.data = data;
+            this.next = null;
         }
     }
-    public static void main(String[] args){
-        Node n1 = new Node(10);
-        Node n2 = new Node(20);
-        Node n3 = new Node(30);
-        Node n4 = new Node(40);
-        Node n5 = new Node(50);
+    public static Node head;
+    public static Node tail;
+    public static int size;
 
-        n1.next = n2;
-        n2.next = n3;
-        n3.next = n4;
-        n4.next = n5;
-
-        Node head = n1;
-        head = printLL(head);
+    //methods
+    public void addFirst(int data){  //O(1)
+        //create new node
+        Node newNode = new Node(data);
+        size++;
+        if(head==null){
+            head = tail = newNode;
+            return;
+        }
+        //newNode next = head
+        newNode.next = head;
+        //head = newNode;
+        head = newNode;
+    }
+    public void addLast(int data){ //O(1)
+        Node newNode = new Node(data);
+        size++;
+        if(head == null){
+            head = tail = newNode;
+            return;
+        }
+        tail.next = newNode;
+        tail = newNode;
+    }
+    public void print(){  //O(n)
         Node temp = head;
         while(temp!=null){
-            System.out.println(temp.data+" ");
-            temp=temp.next;
+            System.out.print(temp.data+"->");
+            temp = temp.next;
         }
+        System.out.println("null");
     }
-    //print linkedlist
-    public static Node printLL(Node head){
-        while(head!=null){
-            System.out.println(head.data);
-            head = head.next;
+    public void addMid(int idx, int data){
+        if(idx==0){
+            addFirst(data);
+            return;
         }
-        return head;
+        Node newNode = new Node(data);
+        size++;
+        Node temp = head;
+        int i=0;
+        while(i < idx-1){
+            temp = temp.next;
+            i++;
+        }
+        newNode.next = temp.next;
+        temp.next = newNode;
+    }
+    public int removeFirst(){
+        if(size == 0){
+            System.out.println("LL is empty");
+            return Integer.MIN_VALUE;
+        }else if(size == 1){
+            int val = head.data;
+            head = tail = null;
+            size = 0;
+            return val;
+        }
+        int val = head.data;
+        head = head.next;
+        size--;
+        return val;
+    }
+    public int removeLast(){
+        if(size == 0){
+            System.out.println("LL is Empty");
+            return Integer.MIN_VALUE;
+        }else if(size == 1){
+            int val = head.data;
+            head = tail = null;
+            size = 0;
+            return val;
+        }
+        //prev : i = size -2;
+        Node prev = head;
+        for(int i=0; i<size-2; i++){
+            prev = prev.next;
+        }
+        int val = prev.next.data;
+        prev.next = null;
+        tail = prev;
+        size--;
+        return val;
+    }
+    public int itrSearch(int key){
+        Node temp = head;
+        int i=0;
+        while(temp != null){
+            if(temp.data == key){
+                return i;
+            }
+            temp = temp.next;
+            i++;
+        }
+        return -1;
+    }
+    public int helper(Node head, int key){ //O(n)
+        if(head == null){
+            return -1;
+        }
+        if(head.data == key){
+            return 0;
+        }
+        int idx = helper(head.next, key);
+        if(idx == -1){
+            return -1;
+        }
+        return idx+1;
+    }
+    public int recSearch(int key){
+        return helper(head, key);
     }
 
-    //Search K in Linked list
-//ERROR
-//    public static boolean search(Node head, int k){
-//        Node temp = head;
-//        while(temp!=null){
-//            if(temp.data==k){
-//                return true;
-//            }
-//            temp = temp.next;
-//        }
-//        return false;
-//    }
+    public void reverse(){
+        Node prev = null;
+        Node curr = tail = head;
+        Node next;
+        while(curr!=null){
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        head = prev;
+    }
 
-    //Length of LL
-    //ERROR
-//    int lengthLL(Node head){
-//        Node temp = head;
-//        int count = 0;
-//        while(temp!=null){
-//            count++;
-//            temp=temp.next;
-//        }
-//       return count;
-//    }
+    public void deleteNthfromEnd(int n){
+        //calculate size
+        int sz = 0;
+        Node temp = head;
+        while(temp != null){
+            temp = temp.next;
+            sz++;
+        }
+        if(n==sz){
+            head = head.next; //removeFirst
+            return;
+        }
+        //sz-n
+        int i=1;
+        int iToFind = sz-n;
+        Node prev = head;
+        while(i < iToFind){
+            prev = prev.next;
+            i++;
+        }
+        prev.next = prev.next.next;
+        return;
+    }
+    public static void main(String[] args) {
+        LinkedList ll = new LinkedList();
+        ll.print();
+        ll.addFirst(2);
+        ll.addFirst(1);
+        ll.addLast(3);
+        ll.addLast(4);
+        ll.addMid(2,9);
+        ll.print();
+        System.out.println(ll.size);
+        ll.removeFirst();
+        ll.print();
+        ll.removeLast();
+        ll.print();
+        System.out.println(ll.size);
+        System.out.println(ll.recSearch(9));
+        ll.reverse();
+        ll.print();
+        ll.deleteNthfromEnd(2);
+        ll.print();
+    }
 }
