c219d1f3b63c0978e275c0e22ac9cb5d06dca888
Prateek Kumar <71647878+geek-prateek@users.noreply.github.com>
2023-06-27 00:04:49 +0530

    Add files via upload

--- DSA/BinaryTrees/LCA.java
+++ DSA/BinaryTrees/LCA.java
@@ -47,6 +47,23 @@ public class LCA {
         Node lca = path1.get(i-1);
         return lca;
     }
+    //Approach 2
+    public static Node lca2(Node root, int n1, int n2){
+        if(root == null || root.data == n1 || root.data == n2){
+            return root;
+        }
+        Node left = lca2(root.left, n1, n2);
+        Node right = lca2(root.right, n1, n2);
+
+        //leftLCA = val rightLCA = null
+        if(right == null){
+            return left;
+        }
+        if(left == null){
+            return right;
+        }
+        return root;
+    }
     public static void main(String[] args) {
         Node root = new Node(1);
         root.left = new Node(2);
@@ -56,7 +73,7 @@ public class LCA {
         root.right.left = new Node(6);
         root.right.right = new Node(7);
 
-        int n1 = 4, n2 = 5;
-        System.out.println(lca(root, n1, n2).data);
+        int n1 = 4, n2 = 7;
+        System.out.println(lca2(root, n1, n2).data);
     }
 }
